3a8e6a094833ce1c59214213d90e64afba09a7e4
lnicanor <76955271+lnicanor@users.noreply.github.com>
2022-11-14 23:13:40 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,41 +1,38 @@
-var randomWord = ''
 var canvas = document.getElementById('myCanvas')
 var ctx = canvas.getContext('2d')
-const usr_input = document.getElementById('submit').value
+ctx.font = '30px Gloria Hallelujah'
+ctx.fillStyle = 'blue'
+ctx.textAlign = 'center'
+
+var uniquechar = ''
 
 function generate () {
-  //Limpiamos el input y el cambas cuando se genera una nueva palabra
   document.getElementById('submit').value = ''
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-  //Propiedades para la palabra a generar
-  ctx.font = '30px Gloria Hallelujah'
-  ctx.fillStyle = 'blue'
-  ctx.textAlign = 'center'
+  const randomchar =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}|":'
 
-  //Constante para generar la palabra con las letras y signos del string
-  const randomChar =
-    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}|<>?'
-
-  //Generamos una palabra de 5 letras a partir del string dato
   for (let i = 1; i < 5; i++) {
-    randomWord += randomChar.charAt(Math.random() * randomChar.length)
+    uniquechar += randomchar.charAt(Math.random() * randomchar.length)
   }
+  ctx.fillText(uniquechar, canvas.width / 2, canvas.height / 2)
 
-  //Utilizamos el metodo 'fillText' para generar la palabra en el canvas
-  ctx.fillText(randomWord, canvas.width / 2, canvas.height / 2)
+  console.log(uniquechar)
 }
 
-//llamamos a la funcion que genera la palabra
 generate()
 
 function printmsg () {
-  if (usr_input == randomWord) {
+  const usr_input = document.getElementById('submit').value
+
+  // Check whether the input is equal
+  // to generated captcha or not
+  if (usr_input == uniquechar) {
     document.getElementById('key').innerHTML = 'Correcto'
     generate()
   } else {
-    document.getElementById('key').innerHTML =
-      'La entrada no corresponde con el captcha'
+    document.getElementById('key').innerHTML = 'La entrada no coincide'
     generate()
   }
 }
